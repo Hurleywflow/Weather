5cefdf781301db610c2ebd6fd343129873b791e5
Hurley <86563399+Hurleywflow@users.noreply.github.com>
2022-06-07 12:19:53 +1000

    weather app init

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,23 @@
-const key = 'Mtu2CuG0qcGeSYG0MGyOnV1NgqgxPk1A';
+const key = '	Mtu2CuG0qcGeSYG0MGyOnV1NgqgxPk1A';
+
+// get weather information
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
