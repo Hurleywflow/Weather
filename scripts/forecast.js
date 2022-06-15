6a917952da07eefb09458c15355627d438feeef6
Hurley <86563399+Hurleywflow@users.noreply.github.com>
2022-06-15 17:13:18 +1000

    done weather app with update https for URI

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,9 +2,9 @@ class Forecast {
   constructor() {
     this.key = 'Mtu2CuG0qcGeSYG0MGyOnV1NgqgxPk1A';
     this.weatherURI =
-      'http://dataservice.accuweather.com/currentconditions/v1/';
+      'https://dataservice.accuweather.com/currentconditions/v1/';
     this.cityURI =
-      'http://dataservice.accuweather.com/locations/v1/cities/search';
+      'https://dataservice.accuweather.com/locations/v1/cities/search';
   }
   async updateCity(city) {
     const cityDets = await this.getCity(city);
